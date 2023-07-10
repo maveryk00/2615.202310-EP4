ea43a69c051d35a155435ac06e7c44bb239c1140
Felipe Jimenez <maveryk00@gmail.com>
2023-07-10 16:43:38 -0500

    Initial commit

---
+++ src/main/java/xyz/maveryk/EP4/Ep4Application.java
@@ -0,0 +1,13 @@
+package xyz.maveryk.EP4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ep4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ep4Application.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/maveryk/EP4/Ep4ApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.maveryk.EP4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ep4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
